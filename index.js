e9ddb74cf5497178642b40c2f15082df5e832ff4
shireesha-palakurla <pshireesha0706@gmail.com>
2025-07-03 12:07:05 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+document.querySelector(".ply").addEventListener("click", () => {
+    var audio = new Audio("dev.mp3");
+    audio.play();
+});
+
+
+Click play button , when you exit the audio gets off.
+
